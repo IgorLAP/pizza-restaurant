d23681abfd89db0aafab71a486fee288c116ad20
Igor Pedrosa <ilap1036@gmail.com>
2022-05-13 10:36:13 -0300

    style: navbar implementation

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head><title>Pizza Time | Order the best pizzas in Next.js</title></Head>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Roboto+Flex:opsz,wght@8..144,400;8..144,700&display=swap" rel="stylesheet" />
+        <link rel="shortcut icon" href="./favicon.png" type="image/png" />
+        </Head>
       <body>
         <Main />
         <NextScript />
--- frontend/src/pages/components/Navbar/index.tsx
+++ frontend/src/pages/components/Navbar/index.tsx
@@ -1,9 +1,44 @@
-import styles from './styles.module.scss';
+import Head from 'next/head'
+import Image from 'next/image'
+
+import styles from './styles.module.scss'
 
 export function Navbar() {
   return (
-    <div>
-
+    <>
+      <Head>
+        <title>Pizza Time | Order the best pizzas in Next.js</title>
+      </Head>
+      <div className={styles.container}>
+      <div className={styles.item}>
+        <button type='button'>
+          <Image width={32} height={32} src="/img/telephone.png" alt='call us' />
+        </button>
+        <div className={styles.callText}>
+          <span>ORDER NOW!</span>
+          <span>012 345 678</span>
+        </div>
+      </div>
+      <div className={styles.item}>
+        <nav>
+          <ul>
+            <li>Homepage</li>
+            <li>Products</li>
+            <li>Menu</li>
+            <Image src='/img/logo.png' alt='Pizza time logo' width={160} height={69} />
+            <li>Events</li>
+            <li>Blog</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
+      </div>
+      <div className={styles.item}>
+        <div className={styles.cart}>
+          <Image src='/img/cart.png' alt='Cart' width={30} height={30} />
+          <span className={styles.counter}>2</span>
+        </div>
+      </div>
     </div>
+    </>
   )
 }
