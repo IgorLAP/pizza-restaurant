5537b050a533f0cac2c34e6680b6cd986fdc408c
Igor Pedrosa <ilap1036@gmail.com>
2022-05-13 09:48:44 -0300

    chore: main configuration and setup

---
+++ frontend/src/pages/_document.tsx
@@ -0,0 +1,13 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head><title>Pizza Time | Order the best pizzas in Next.js</title></Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
