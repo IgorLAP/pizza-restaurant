73fa90affbd4f71b4974df64674ce2ffc7317770
Igor Pedrosa <ilap1036@gmail.com>
2022-05-19 14:23:52 -0300

    feat: next.js functions and data from mongoDB

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,33 @@
+import { GetStaticProps } from 'next'
 import Head from 'next/head'
+
 import { PizzaList } from '../components/PizzaList'
 import { Slider } from '../components/Slider'
+import { ProductInterface } from '../interfaces/ProductInterface'
+import { getProducts } from '../lib/get-products'
 
-export default function Home() {
+interface HomeProps {
+  products: ProductInterface[]
+}
+
+export default function Home({ products }: HomeProps) {
   return (
     <>
       <Head><title>Pizza Time | Order the best pizzas with Next.js</title></Head>
       <Slider />
-      <PizzaList />
+      <PizzaList list={products} />
     </>
   )
+}
+
+
+export const getStaticProps: GetStaticProps = async () => {
+  const products: ProductInterface[] = await getProducts()
+
+  return {
+    props: {
+      products: JSON.parse(JSON.stringify(products))
+    },
+    revalidate: 60 * 60
+  }
 }
--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,39 +1,59 @@
+import { GetServerSideProps } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
+import { Extras, ProductInterface } from '../../interfaces/ProductInterface'
+import { getOneProduct } from '../../lib/get-one-product'
 import styles from './style.module.scss'
 
-export default function Product() {
+interface ProductProps {
+  product: ProductInterface;
+}
+
+export default function Product({ product }: ProductProps) {
   const [sizeSelected, setSizeSelected] = useState(0)
   const [quantity, setQuantity] = useState(1)
+  const [extraOption, setExtraOption] = useState<Extras[]>([{ text: '', price: 0 }])
+  const [actualPrice, setActualPrice] = useState(0)
+  
+  useEffect(() => {
+    setActualPrice(() => {
+      const extras = extraOption.reduce((total, item) => {
+        total += item.price
+        return total
+      }, 0)
+      const actual = [product.prices[sizeSelected], quantity, extras];
+      return (actual[0] * actual[1]) + actual[2]
+    })
+  }, [sizeSelected, quantity, extraOption])
 
-  const pizzaPlaceholder = {
-    id: 1,
-    img: '/img/pizza.png',
-    name: 'CAMPAGNOLA',
-    price: [19.9, 23.9, 27.9],
-    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem fugit labore dolores doloribus fugiat, atque, ullam quis inventore reiciendis facere nobis mollitia tempora numquam eaque velit rerum officiis quia amet.'
+  function handleInputChange(e: React.ChangeEvent<HTMLInputElement>, item: Extras) {
+    if(e.target.checked) {
+      setExtraOption([...extraOption, item])
+    } else {
+      setExtraOption(prevState => prevState.filter(i => i.text !== item.text))
+    }
   }
 
   return (
     <>
-    <Head><title>{pizzaPlaceholder.name} | Pizza Time</title></Head>
+    <Head><title>{product.title} | Pizza Time</title></Head>
       <div className={styles.container}>
         <div className={styles.leftSide}>
           <div className={styles.imgContainer}>
-            <Image src={pizzaPlaceholder.img} alt="pizza" layout='fill' objectFit='contain' />
+            <Image src={product.img} alt="pizza" layout='fill' objectFit='contain' />
           </div>
         </div>
         <div className={styles.rightSide}>
-          <h1>{pizzaPlaceholder.name}</h1>
+          <h1>{product.title}</h1>
           <div className={styles.price}>
             <span>{new Intl.NumberFormat('en-US', {
               currency: 'USD',
               style: 'currency'
-            }).format(pizzaPlaceholder.price[sizeSelected])}</span>
+            }).format(actualPrice)}</span>
           </div>
-          <p className={styles.desc}>{pizzaPlaceholder.desc}</p>
+          <p className={styles.desc}>{product.desc}</p>
           <h3>Choose the size</h3>
           <div className={styles.pizzaSize}>
             <div onClick={() =>  setSizeSelected(0)} className={styles.size} >
@@ -51,14 +71,15 @@ export default function Product() {
           </div>
           <h3>Choose the aditional ingredients</h3>
           <div className={styles.aditionalArea}>
-            <input type="checkbox" name="double" id="double" />
-            <label htmlFor="double">Double ingredients</label>
-            <input type="checkbox" name="extra" id="extra" />
-            <label htmlFor="extra">Extra cheese</label>
-            <input type="checkbox" name="spicy" id="spicy" />
-            <label htmlFor="spicy">Spicy sauce</label>
-            <input type="checkbox" name="garlic" id="garlic" />
-            <label htmlFor="garlic">Garlic sauce</label>
+            {product.extraOptions.map((item) => (
+              <>
+                <input
+                  onChange={(e) => handleInputChange(e, item)} 
+                  type="checkbox" name={item.text} id={item.text} 
+                />
+                <label htmlFor={item.text}>{item.text}</label>
+              </>
+            ))}
           </div>
           <div className={styles.final}>
             <input type="number" value={quantity} onChange={(e) => setQuantity(Number(e.target.value))} />
@@ -68,4 +89,15 @@ export default function Product() {
       </div>
     </>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+  const { id } = params;
+  const product = await getOneProduct(id as string)
+
+  return {
+    props: {
+      product: JSON.parse(JSON.stringify(product))
+    }
+  }
 }
