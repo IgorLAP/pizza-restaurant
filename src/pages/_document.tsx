cdede3be933a5ab15abbc51511ce7c65e367ddc2
Igor Pedrosa <ilap1036@gmail.com>
2022-05-22 12:54:37 -0300

    feat: paypal sdk script in document instead of react-paypal-js

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,7 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Roboto+Flex:opsz,wght@8..144,100;8..144,400;8..144,700&display=swap" rel="stylesheet" />
         <link rel="shortcut icon" href="/favicon.png" type="image/png" />
+        <script defer src="https://www.paypal.com/sdk/js?client-id=AeiwSgb_kVCY7spRA18zoF6tiT25WM8JHTntIYGnrnx2Ah8acZ9ntgO-wgFk5eASDufRAKBHrbj_Qhus&currency=USD"></script>
       </Head>
       <body>
         <Main />
