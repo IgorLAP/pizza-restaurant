20040fdc8ae08c14890fb4c5d2d4914659531dbe
Igor Pedrosa <ilap1036@gmail.com>
2022-05-25 10:51:11 -0300

    refactor: responsive and helper alert functions

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -2,9 +2,9 @@ import axios from 'axios'
 import { GetServerSideProps } from 'next'
 import Image from 'next/image'
 import { useState } from 'react'
-import Swal from 'sweetalert2'
 
 import { moneyFormatter } from '../../helpers/moneyFormatter'
+import { showConfirmAlert, showSimpleErrorAlert, showSimpleSuccessAlert } from '../../helpers/showAlert'
 import { OrderInterface } from '../../interfaces/OrderInterface'
 import { ProductInterface } from '../../interfaces/ProductInterface'
 import Orders from '../../models/Orders'
@@ -22,45 +22,23 @@ export default function Admin({ orders, products }: AdminProps) {
   const status = [ 'Preparing', 'On the way', 'Delivered' ]
 
   function handleDeleteProduct(id: string) {
-    Swal.fire({
-      title: `Delete product with id:${id.slice(0,5)}...?`,
-      text: "You won't be able to revert this!",
-      icon: 'warning',
-      showCancelButton: true,
-      confirmButtonColor: '#30d643',
-      cancelButtonColor: '#d33',
-      confirmButtonText: 'Yes, delete it!'
-    }).then((result) => {
+    showConfirmAlert({ id })
+    .then((result) => {
       if (result.isConfirmed) {
         try {
           axios.delete(`/api/products/${id}`)
           setPizzaList(prevState => prevState.filter(i => i._id !== id))
-          Swal.fire(
-            'Deleted!',
-            'Your file has been deleted.',
-            'success'
-          )
+          showSimpleSuccessAlert('Deleted', 'Your file has been deleted.')
         } catch (err) {
-          Swal.fire(
-            'Error!',
-            `Something went wrong! ${err}`,
-            'error'
-          )
+          showSimpleErrorAlert('Error!', `Something went wrong! ${err.message}`)
         }
       }
     })
   }
 
   function handleNextStage({ id, status }: { id: string, status: number }) {
-    Swal.fire({
-      title: `Order id:${id.slice(0,5)}... to the next stage?`,
-      text: "You won't be able to revert this!",
-      icon: 'warning',
-      showCancelButton: true,
-      confirmButtonColor: '#30d643',
-      cancelButtonColor: '#d33',
-      confirmButtonText: 'Yes, change stage!'
-    }).then((result) => {
+    showConfirmAlert({ title: 'Confirm next stage?', confirmBtnTxt: 'Confirm' })
+    .then((result) => {
       if (result.isConfirmed) {
         try {
           axios.put('/api/orders', { id, status })
@@ -72,17 +50,9 @@ export default function Admin({ orders, products }: AdminProps) {
             }
             setOrdersList(tmpArr)
           })
-          Swal.fire(
-            'Success!',
-            'Status changed',
-            'success'
-          )
+          showSimpleSuccessAlert('Success!', 'Status changed')
         } catch (err) {
-          Swal.fire(
-            'Error!',
-            `Something went wrong! ${err}`,
-            'error'
-          )
+          showSimpleErrorAlert('Error!', `Something went wrong! ${err.message}`)
         }
       }
     })
@@ -92,86 +62,90 @@ export default function Admin({ orders, products }: AdminProps) {
     <div className={styles.container}>
       <div className={styles.leftTable}>
         <h2>Products</h2>
-        <table>
-          <thead>
-            <tr>
-              <th>Image</th>
-              <th>Id</th>
-              <th>Title</th>
-              <th>Prices</th>
-              <th>Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {pizzaList.map(item => (
-              <tr key={item._id}>
-                <td>
-                  <Image 
-                    src={item.img}
-                    width={60}
-                    height={60}
-                    alt={item.title}
-                  />
-                </td>
-                <td>{item._id.slice(0,5)}...</td>
-                <td>{item.title}</td>
-                <td>
-                  {item.prices.map((price, index) => (
-                    <span 
-                      style={{ display: 'block', marginBottom: '4px' }} 
-                      key={price}
-                    >{moneyFormatter(price)}</span>
-                  ))}
-                </td>
-                <td>
-                  <button className={styles.editBtn}>edit</button>
-                  <button 
-                    onClick={() => handleDeleteProduct(item._id)}
-                    className={styles.delBtn}
-                  >delete</button>
-                </td>
+        <div className={styles.tableWrapper}>
+          <table>
+            <thead>
+              <tr>
+                <th>Image</th>
+                <th>Id</th>
+                <th>Title</th>
+                <th>Prices</th>
+                <th>Actions</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {pizzaList.map(item => (
+                <tr key={item._id}>
+                  <td>
+                    <Image
+                      src={item.img}
+                      width={60}
+                      height={60}
+                      alt={item.title}
+                    />
+                  </td>
+                  <td>{item._id.slice(0,5)}...</td>
+                  <td>{item.title}</td>
+                  <td>
+                    {item.prices.map((price, index) => (
+                      <span
+                        style={{ display: 'block', marginBottom: '4px' }}
+                        key={price}
+                      >{moneyFormatter(price)}</span>
+                    ))}
+                  </td>
+                  <td>
+                    <button className={styles.editBtn}>edit</button>
+                    <button
+                      onClick={() => handleDeleteProduct(item._id)}
+                      className={styles.delBtn}
+                    >delete</button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
       <div className={styles.rightTable}>
         <h2>Orders</h2>
-        <table>
-          <thead>
-            <tr>
-              <th>Id</th>
-              <th>Customer</th>
-              <th>Total</th>
-              <th>Payment</th>
-              <th>Status</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            {ordersList.map(item => (
-              <tr key={item._id}>
-                <td>{item._id.slice(0,5)}...</td>
-                <td>{item.customer}</td>
-                <td>{moneyFormatter(item.total)}</td>
-                <td>{item.method == 0 ? 'Cash on delivery' : 'Paypal'}</td>
-                <td>{status[item.status]}</td>
-                <td>
-                  {status[item.status] == 'Delivered' ? (
-                    <button 
-                      style={{ padding: '.4rem', opacity: '.5' }} 
-                      disabled 
-                    >Delivered</button>
-                  ) : (
-                    <button 
-                    onClick={() => handleNextStage({ id: item._id, status: item.status + 1 })}
-                  >Next stage</button>
-                  )}
-                </td>
+        <div className={styles.tableWrapper}>
+          <table>
+            <thead>
+              <tr>
+                <th>Id</th>
+                <th>Customer</th>
+                <th>Total</th>
+                <th>Payment</th>
+                <th>Status</th>
+                <th>Action</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {ordersList.map(item => (
+                <tr key={item._id}>
+                  <td>{item._id.slice(0,5)}...</td>
+                  <td>{item.customer}</td>
+                  <td>{moneyFormatter(item.total)}</td>
+                  <td>{item.method == 0 ? 'Cash on delivery' : 'Paypal'}</td>
+                  <td>{status[item.status]}</td>
+                  <td>
+                    {status[item.status] == 'Delivered' ? (
+                      <button
+                        style={{ padding: '.4rem', opacity: '.5' }}
+                        disabled
+                      >Delivered</button>
+                    ) : (
+                      <button
+                      onClick={() => handleNextStage({ id: item._id, status: item.status + 1 })}
+                    >Next stage</button>
+                    )}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   )
