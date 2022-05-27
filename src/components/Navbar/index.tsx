2e54b2552666258eabc6859004d648f492c8cfcd
Igor Pedrosa <ilap1036@gmail.com>
2022-05-27 09:48:21 -0300

    refactor: mobileMenu proper behaviour and style

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,18 +1,28 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import { useRef, useState } from 'react'
 
 import { useAppSelector } from '../../hooks/useAppSelector'
 import styles from './styles.module.scss'
 
+
 export function Navbar() {
   const quantity = useAppSelector(state => state.cart.quantity)
+  const input = useRef<any>()
+
+  const [display, setDisplay] = useState(false)
+  
+  function handleLinkClick() {
+    if(input.current.checked) input.current.checked = false
+    if (display) setDisplay(false)
+  }
 
   return (
     <div className={styles.container}>
       <div className={styles.item}>
         <div className={styles.mobileMenu}>
           <div className={styles.menuContainer}>
-            <input type="checkbox" id='checkbox-menu' className={styles.checkboxMenu} />
+            <input ref={input} onClick={() => setDisplay(prevState => !prevState)} type="checkbox" id='checkbox-menu' className={styles.checkboxMenu} />
             <label htmlFor="checkbox-menu">
               <span></span>
               <span></span>
@@ -28,15 +38,15 @@ export function Navbar() {
           <span>012 345 678</span>
         </div>
       </div>
-      <div className={styles.item}>
+      <div style={{ display: display ? 'block' : '' }} className={styles.item}>
         <nav>
           <ul>
             <li>
-              <Link href='/'>Homepage</Link>
+              <Link href='/'><span onClick={handleLinkClick}>Homepage</span></Link>
             </li>
             <li>Products</li>
             <li>
-              <Link href='/login'>Login</Link>
+              <Link href='/login'><span onClick={handleLinkClick}>Login</span></Link>
             </li>
             <Image src='/img/logo.png' alt='Pizza time logo' width={80} height={80} />
             <li>Events</li>
@@ -47,7 +57,7 @@ export function Navbar() {
       </div>
       <div className={styles.item}>
         <Link href='/cart'>
-          <div className={styles.cart}>
+          <div onClick={handleLinkClick} className={styles.cart}>
             <Image src='/img/cart.png' alt='Cart' width={30} height={30} />
             <span className={styles.counter}>{quantity}</span>
           </div>
