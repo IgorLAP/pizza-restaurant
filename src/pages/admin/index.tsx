73b38284d1d05b336cc811bbbf0e3ca485232263
IgorLAP <ilap1036@gmail.com>
2022-06-02 13:11:47 -0300

    refactor: minor UX changes

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import { GetServerSideProps } from 'next'
+import Head from 'next/head'
 import Image from 'next/image'
 import { useEffect, useState } from 'react'
 
@@ -20,167 +21,170 @@ interface AdminProps {
 
 export default function Admin({ orders, products }: AdminProps) {
   const [pizzaList, setPizzaList] = useState<ProductInterface[]>(products)
-  
+
   const [ordersList, setOrdersList] = useState(orders)
   const [modal, setModal] = useState(false)
   const [editProduct, setEditProduct] = useState<ProductInterface>()
 
-  const status = [ 'Preparing', 'On the way', 'Delivered' ]
+  const status = ['Preparing', 'On the way', 'Delivered']
 
   useEffect(() => {
     if (!modal) {
       axios.get('/api/products')
-      .then(res => {
-        setPizzaList(res.data as ProductInterface[])
-      })
-      .catch(err => showToast('Unable to verify new product, please refresh', 'error'))
+        .then(res => {
+          setPizzaList(res.data as ProductInterface[])
+        })
+        .catch(err => showToast('Unable to verify new product, please refresh', 'error'))
     }
   }, [modal])
 
   function handleDeleteProduct(id: string) {
     showConfirmAlert({ id })
-    .then((result) => {
-      if (result.isConfirmed) {
-        try {
-          axios.delete(`/api/products/${id}`)
-          setPizzaList(prevState => prevState.filter(i => i._id !== id))
-          showSimpleSuccessAlert('Deleted', 'Your file has been deleted.')
-        } catch (err) {
-          showSimpleErrorAlert('Error!', `Something went wrong! ${err.message}`)
+      .then((result) => {
+        if (result.isConfirmed) {
+          try {
+            axios.delete(`/api/products/${id}`)
+            setPizzaList(prevState => prevState.filter(i => i._id !== id))
+            showSimpleSuccessAlert('Deleted', 'Your file has been deleted.')
+          } catch (err) {
+            showSimpleErrorAlert('Error!', `Something went wrong! ${err.message}`)
+          }
         }
-      }
-    })
+      })
   }
 
   function handleNextStage({ id, status }: { id: string, status: number }) {
     showConfirmAlert({ title: 'Confirm next stage?', confirmBtnTxt: 'Confirm' })
-    .then((result) => {
-      if (result.isConfirmed) {
-        try {
-          axios.put('/api/orders', { id, status })
-          .then(res => {
-            const tmpArr = [...ordersList]
-            const index = tmpArr.findIndex(i => i._id == id)
-            if(index !== -1) {
-              tmpArr[index] = res.data as OrderInterface
-            }
-            setOrdersList(tmpArr)
-          })
-          showSimpleSuccessAlert('Success!', 'Status changed')
-        } catch (err) {
-          showSimpleErrorAlert('Error!', `Something went wrong! ${err.message}`)
+      .then((result) => {
+        if (result.isConfirmed) {
+          try {
+            axios.put('/api/orders', { id, status })
+              .then(res => {
+                const tmpArr = [...ordersList]
+                const index = tmpArr.findIndex(i => i._id == id)
+                if (index !== -1) {
+                  tmpArr[index] = res.data as OrderInterface
+                }
+                setOrdersList(tmpArr)
+              })
+            showSimpleSuccessAlert('Success!', 'Status changed')
+          } catch (err) {
+            showSimpleErrorAlert('Error!', `Something went wrong! ${err.message}`)
+          }
         }
-      }
-    })
+      })
   }
 
   return (
-    <div className={styles.container}>
-      <div className={styles.leftTable}>
-        <h2>Products</h2>
-        <button 
-          onClick={() => setModal(true)} 
-          className={styles.addNew}>Add new product</button>
-        {modal && 
-          <ProductModal product={editProduct} setModal={setModal} />
-        }
-        <div className={styles.tableWrapper}>
-          <table>
-            <thead>
-              <tr>
-                <th>Image</th>
-                <th>Id</th>
-                <th>Title</th>
-                <th>Prices</th>
-                <th>Actions</th>
-              </tr>
-            </thead>
-            <tbody>
-              {pizzaList.map(item => (
-                <tr key={item._id}>
-                  <td>
-                    <Image
-                      src={item.img}
-                      width={60}
-                      height={60}
-                      alt={item.title}
-                    />
-                  </td>
-                  <td>{item._id.slice(0,5)}...</td>
-                  <td>{item.title}</td>
-                  <td>
-                    {item.prices.map((price, index) => (
-                      <span
-                        style={{ display: 'block', marginBottom: '4px' }}
-                        key={price}
-                      >{moneyFormatter(price)}</span>
-                    ))}
-                  </td>
-                  <td>
-                    <button 
-                      onClick={() => {
-                        setEditProduct(item)
-                        setModal(true)
-                      }}
-                      className={styles.editBtn}>edit</button>
-                    <button
-                      onClick={() => handleDeleteProduct(item._id)}
-                      className={styles.delBtn}
-                    >delete</button>
-                  </td>
+    <>
+      <Head><title>Admin Dashboard | Pizza Time</title></Head>
+      <div className={styles.container}>
+        <div className={styles.leftTable}>
+          <h2>Products</h2>
+          <button
+            onClick={() => setModal(true)}
+            className={styles.addNew}>Add new product</button>
+          {modal &&
+            <ProductModal product={editProduct} setModal={setModal} />
+          }
+          <div className={styles.tableWrapper}>
+            <table>
+              <thead>
+                <tr>
+                  <th>Image</th>
+                  <th>Id</th>
+                  <th>Title</th>
+                  <th>Prices</th>
+                  <th>Actions</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
+              </thead>
+              <tbody>
+                {pizzaList.map(item => (
+                  <tr key={item._id}>
+                    <td>
+                      <Image
+                        src={item.img}
+                        width={60}
+                        height={60}
+                        alt={item.title}
+                      />
+                    </td>
+                    <td>{item._id.slice(0, 5)}...</td>
+                    <td>{item.title}</td>
+                    <td>
+                      {item.prices.map((price, index) => (
+                        <span
+                          style={{ display: 'block', marginBottom: '4px' }}
+                          key={price}
+                        >{moneyFormatter(price)}</span>
+                      ))}
+                    </td>
+                    <td>
+                      <button
+                        onClick={() => {
+                          setEditProduct(item)
+                          setModal(true)
+                        }}
+                        className={styles.editBtn}>edit</button>
+                      <button
+                        onClick={() => handleDeleteProduct(item._id)}
+                        className={styles.delBtn}
+                      >delete</button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+        <div className={styles.rightTable}>
+          <h2>Orders</h2>
+          <div className={styles.tableWrapper}>
+            <table>
+              <thead>
+                <tr>
+                  <th>Id</th>
+                  <th>Customer</th>
+                  <th>Total</th>
+                  <th>Payment</th>
+                  <th>Status</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {ordersList.map(item => (
+                  <tr key={item._id}>
+                    <td>{item._id.slice(0, 5)}...</td>
+                    <td>{item.customer}</td>
+                    <td>{moneyFormatter(item.total)}</td>
+                    <td>{item.method == 0 ? 'Cash on delivery' : 'Paypal'}</td>
+                    <td>{status[item.status]}</td>
+                    <td>
+                      {status[item.status] == 'Delivered' ? (
+                        <button
+                          style={{ padding: '.4rem', opacity: '.5' }}
+                          disabled
+                        >Delivered</button>
+                      ) : (
+                        <button
+                          onClick={() => handleNextStage({ id: item._id, status: item.status + 1 })}
+                        >Next stage</button>
+                      )}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
-      <div className={styles.rightTable}>
-        <h2>Orders</h2>
-        <div className={styles.tableWrapper}>
-          <table>
-            <thead>
-              <tr>
-                <th>Id</th>
-                <th>Customer</th>
-                <th>Total</th>
-                <th>Payment</th>
-                <th>Status</th>
-                <th>Action</th>
-              </tr>
-            </thead>
-            <tbody>
-              {ordersList.map(item => (
-                <tr key={item._id}>
-                  <td>{item._id.slice(0,5)}...</td>
-                  <td>{item.customer}</td>
-                  <td>{moneyFormatter(item.total)}</td>
-                  <td>{item.method == 0 ? 'Cash on delivery' : 'Paypal'}</td>
-                  <td>{status[item.status]}</td>
-                  <td>
-                    {status[item.status] == 'Delivered' ? (
-                      <button
-                        style={{ padding: '.4rem', opacity: '.5' }}
-                        disabled
-                      >Delivered</button>
-                    ) : (
-                      <button
-                      onClick={() => handleNextStage({ id: item._id, status: item.status + 1 })}
-                    >Next stage</button>
-                    )}
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      </div>
-    </div>
+    </>
   )
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ req }) => {
   const { token } = req.cookies;
-  
+
   if (!token || token !== process.env.TOKEN) {
     return {
       redirect: {
@@ -189,7 +193,7 @@ export const getServerSideProps: GetServerSideProps = async ({ req }) => {
       }
     }
   }
-  
+
   const orders = await Orders.find();
   const products = await Product.find();
 
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import { GetServerSideProps } from 'next'
+import Head from 'next/head'
 import { useRouter } from 'next/router'
 import { useState } from 'react'
 
@@ -16,7 +17,7 @@ export default function Login() {
   const router = useRouter()
 
   async function handleLogin() {
-    if (username !== '' && password !== '')  {
+    if (username !== '' && password !== '') {
       setOnRequest(true)
       try {
         const res = await axios.post('/api/login', { username, password })
@@ -29,7 +30,7 @@ export default function Login() {
           setOnRequest(false)
           showSimpleErrorAlert('Error!', 'E-mail or password is wrong')
         }
-      } catch(err) {
+      } catch (err) {
         setOnRequest(false)
         showSimpleErrorAlert('Error!', `Something went wrong: ${err.message}`)
       }
@@ -41,39 +42,42 @@ export default function Login() {
 
 
   return (
-    <div className={styles.container}>
-      <h1>Admin Login</h1>
-      <div className={styles.inputField}>
-        <label htmlFor="username">Username</label>
-        <input 
-          onChange={e => {
-            if (showErrors && msgError !== '') {
-              setShowErrors(false)
-              setMsgError('')
-            }
-            setUsername(e.target.value)
-          }} 
-          type="text" name="username" />
-      </div>
-      <div className={styles.inputField}>
-        <label htmlFor="password">Password</label>
-        <input 
-          onChange={e => {
-            if (showErrors && msgError !== '') {
-              setShowErrors(false)
-              setMsgError('')
-            }
-            setPassword(e.target.value)
-          }} 
-          type="password" name="password" />
-      </div>
-      {showErrors && 
-        <div className={styles.errors}>
-          <span>{msgError}</span>
+    <>
+      <Head><title>Login | Pizza Time</title></Head>
+      <div className={styles.container}>
+        <h1>Admin Login</h1>
+        <div className={styles.inputField}>
+          <label htmlFor="username">Username</label>
+          <input
+            onChange={e => {
+              if (showErrors && msgError !== '') {
+                setShowErrors(false)
+                setMsgError('')
+              }
+              setUsername(e.target.value)
+            }}
+            type="text" name="username" />
         </div>
-      }
-      <button disabled={onRequest} onClick={handleLogin}>Login</button>
-    </div>
+        <div className={styles.inputField}>
+          <label htmlFor="password">Password</label>
+          <input
+            onChange={e => {
+              if (showErrors && msgError !== '') {
+                setShowErrors(false)
+                setMsgError('')
+              }
+              setPassword(e.target.value)
+            }}
+            type="password" name="password" />
+        </div>
+        {showErrors &&
+          <div className={styles.errors}>
+            <span>{msgError}</span>
+          </div>
+        }
+        <button disabled={onRequest} onClick={handleLogin}>Login</button>
+      </div>
+    </>
   )
 }
 
